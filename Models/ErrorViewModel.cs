be92a340fdfe01f257dd165b7e635c4ea3b102e7
yingyao <yingyao@gmail.com>
2023-04-13 11:50:23 +0800

    加入專案檔案。

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DemoDotnetVue2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
