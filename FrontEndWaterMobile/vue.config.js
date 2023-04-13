dc5a15414da2ebfec702c323c20a1ae4fc5ba3dd
yingyao <yingyao@gmail.com>
2023-04-13 17:23:19 +0800

    npm run build to wwwroot

--- FrontEndWaterMobile/vue.config.js
+++ FrontEndWaterMobile/vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  //publicPath這參數加入是因為VUE架到IIS上會友虛擬路徑的問題(因為有用到Router所以index.js裡也要設定base等於這路徑)
+  publicPath: './',
+  //導出的網頁靜態檔路徑
+  outputDir: '../wwwroot'
 })
