723c11788db4e4162085f7f15d3bcb57cfaeb2e2
yingyao <yingyao@gmail.com>
2023-04-13 17:08:33 +0800

    rebuild vue2_eslint with error prevention only

--- FrontEndWaterMobile/README.md
+++ FrontEndWaterMobile/README.md
@@ -1,4 +1,4 @@
-# watermobile
+# watermobile_eslintonly
 
 ## Project setup
 ```
--- FrontEndWaterMobile/src/views/HomeView.vue
+++ FrontEndWaterMobile/src/views/HomeView.vue
@@ -1,14 +1,6 @@
 <template>
   <div class="home">
-    <div class="fix">
-      <form class="form-signin">
-        <img class="mb-4" src="../assets/自來水logo.png" alt="自來水公司LOGO" width="72" height="72" />
-        <h1 class="h4 mb-3 font-weight-bold">圖資查詢系統</h1>
-        <h1 class="h5 mb-3 font-weight-bold">(行動版)</h1>
-        <b-form-select class="form-control mb-4 font-weight-bold selectfix" v-model="selected" :options="options"></b-form-select>
-        
-      </form>
-    </div>
+    <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
